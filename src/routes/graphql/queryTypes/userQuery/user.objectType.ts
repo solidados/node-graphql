e54af285221dd7b4eba93fad208eebf594781f0c
solidados <geekbrains.pk@internet.ru>
2024-11-01 22:32:58 +0400

    chore: create user.objectType; TODO: in process to add other props

---
+++ src/routes/graphql/queryTypes/userQuery/user.objectType.ts
@@ -0,0 +1,38 @@
+import {
+  GraphQLFloat,
+  GraphQLNonNull,
+  GraphQLObjectType,
+  GraphQLString,
+} from 'graphql/type/index.js';
+import { UUIDType } from '../../types/uuid.js';
+
+const userObjectType = new GraphQLObjectType({
+  name: 'User',
+  description: 'User Type',
+  fields: () => ({
+    id: {
+      type: UUIDType,
+      description: 'User ID',
+    },
+    name: {
+      type: new GraphQLNonNull(GraphQLString),
+      description: 'User Name',
+    },
+    balance: {
+      type: new GraphQLNonNull(GraphQLFloat),
+      description: 'User balance',
+    },
+    // TODO: add types for profile, posts, subscriptions, according to type User:
+  }),
+});
+
+/*
+* type User {
+  id: UUID!
+  name: String!
+  balance: Float!
+  profile: Profile
+  posts: [Post!]!
+  userSubscribedTo: [User!]!
+  subscribedToUser: [User!]!
+}*/
