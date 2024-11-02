0bd637fd113e4c880a06db8977d326dc8b034391
solidados <geekbrains.pk@internet.ru>
2024-11-02 22:39:12 +0400

    feat; add fields user, userId, memberType and memberTypeId to profileQuery/profile.ObjectType

--- src/routes/graphql/queryTypes/profileQuery/profile.objectType.ts
+++ src/routes/graphql/queryTypes/profileQuery/profile.objectType.ts
@@ -4,8 +4,13 @@ import {
   nonNullableGraphQLBoolean,
   nonNullableGraphQLInt,
 } from '@/routes/graphql/types/nonNullableFields.type.js';
+import userObjectType from '@/routes/graphql/queryTypes/userQuery/user.objectType.js';
+import { Profile } from '@prisma/client';
+import { PrismaContextInterface } from '@/routes/graphql/types/prismaContext.interface.js';
+import memberObjectType from '@/routes/graphql/queryTypes/memberQuery/member.objectType.js';
+import { memberTypeIdEnum } from '@/routes/graphql/queryTypes/memberQuery/member.typeIdEnum.js';
 
-const profileObjectType = new GraphQLObjectType({
+const profileObjectType: GraphQLObjectType = new GraphQLObjectType({
   name: 'Profile',
   description: 'Profile Type',
   fields: () => ({
@@ -21,7 +26,26 @@ const profileObjectType = new GraphQLObjectType({
       type: nonNullableGraphQLInt,
       description: 'Year Of Birth',
     },
-    // TODO: add MemberType
+    user: {
+      type: userObjectType,
+      description: 'User',
+      resolve: async (source: Profile, _: unknown, context: PrismaContextInterface) =>
+        await context.prisma.user.findUnique({ where: { id: source.userId } }),
+    },
+    userId: {
+      type: UUIDType,
+      description: 'User ID',
+    },
+    memberType: {
+      type: memberObjectType,
+      description: 'Member Type',
+      resolve: async (source: Profile, _: unknown, context: PrismaContextInterface) =>
+        await context.loaders.profileLoader.load(source.memberTypeId),
+    },
+    memberTypeId: {
+      type: memberTypeIdEnum,
+      description: 'Member ID Type',
+    },
   }),
 });
 /*
