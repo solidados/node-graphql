38538c365ea34dea0ed166054b64c91c4a35c521
solidados <geekbrains.pk@internet.ru>
2024-11-02 16:49:28 +0400

    feat: add mutations/subscriptions to sub/unsubscribe user to/from author

---
+++ src/routes/graphql/mutations/subscriptions/subscribe.ts
@@ -0,0 +1,27 @@
+import { PrismaContextInterface } from '@/routes/graphql/types/prismaContext.interface.js';
+import { SubscribeInterface } from '@/routes/graphql/mutations/subscriptions/subscriptions.interface.js';
+import userObjectType from '@/routes/graphql/queryTypes/userQuery/user.objectType.js';
+import { nonNullableUUIDType } from '@/routes/graphql/types/nonNullableFields.type.js';
+
+const subscribe = {
+  subscribe: {
+    type: userObjectType,
+    args: {
+      userId: { type: nonNullableUUIDType },
+      authorId: { type: nonNullableUUIDType },
+    },
+    resolve: async (args: SubscribeInterface, context: PrismaContextInterface) =>
+      await context.prisma.user.update({
+        where: { id: args.userId },
+        data: {
+          userSubscribedTo: {
+            create: {
+              authorId: args.authorId,
+            },
+          },
+        },
+      }),
+  },
+};
+
+export default subscribe;
---
+++ src/routes/graphql/mutations/subscriptions/unsubscribe.ts
@@ -0,0 +1,27 @@
+import { UnsubscribeInterface } from '@/routes/graphql/mutations/subscriptions/subscriptions.interface.js';
+import { PrismaContextInterface } from '@/routes/graphql/types/prismaContext.interface.js';
+import { UUIDType } from '@/routes/graphql/types/uuid.js';
+import { nonNullableUUIDType } from '@/routes/graphql/types/nonNullableFields.type.js';
+
+const unsubscribe = {
+  unsubscribe: {
+    type: UUIDType,
+    args: {
+      userId: { type: nonNullableUUIDType },
+      authorId: { type: nonNullableUUIDType },
+    },
+    resolve: async (args: UnsubscribeInterface, context: PrismaContextInterface) => {
+      await context.prisma.subscribersOnAuthors.delete({
+        where: {
+          subscriberId_authorId: {
+            subscriberId: args.userId,
+            authorId: args.authorId,
+          },
+        },
+      });
+      return args.userId;
+    },
+  },
+};
+
+export default unsubscribe;
