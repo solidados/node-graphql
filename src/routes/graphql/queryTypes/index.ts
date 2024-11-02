ec597d9e1ba2945d2279e04d5fd9516d9177de6a
solidados <geekbrains.pk@internet.ru>
2024-11-02 23:10:08 +0400

    feat: create barrel to re-export queryTypes

---
+++ src/routes/graphql/queryTypes/index.ts
@@ -0,0 +1,19 @@
+import { userQuery } from '@/routes/graphql/queryTypes/userQuery/index.js';
+import { postQuery } from '@/routes/graphql/queryTypes/postQuery/index.js';
+import { profileQuery } from '@/routes/graphql/queryTypes/profileQuery/index.js';
+import { memberQuery } from '@/routes/graphql/queryTypes/memberQuery/index.js';
+import { GraphQLObjectType } from 'graphql/type/index.js';
+
+const queryFields = {
+  ...userQuery,
+  ...profileQuery,
+  ...postQuery,
+  ...memberQuery,
+};
+
+const queryTypes = new GraphQLObjectType({
+  name: 'Query Fields',
+  fields: () => queryFields,
+});
+
+export default queryTypes;
