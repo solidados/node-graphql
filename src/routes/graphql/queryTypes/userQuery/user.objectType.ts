173337988c5b9d8375431e29668756ecc6252915
solidados <geekbrains.pk@internet.ru>
2024-11-02 22:01:24 +0400

    feat: add fields: profile, posts, and subscriptions

--- src/routes/graphql/queryTypes/userQuery/user.objectType.ts
+++ src/routes/graphql/queryTypes/userQuery/user.objectType.ts
@@ -1,6 +1,11 @@
-import { GraphQLObjectType } from 'graphql/type/index.js';
+import { GraphQLList, GraphQLObjectType } from 'graphql/type/index.js';
 import { UUIDType } from '@/routes/graphql/types/uuid.js';
 import { nonNullableGraphQLFloat } from '@/routes/graphql/types/nonNullableFields.type.js';
+import profileObjectType from '@/routes/graphql/queryTypes/profileQuery/profile.objectType.js';
+import { Profile, User } from '@prisma/client';
+import { PrismaContextInterface } from '@/routes/graphql/types/prismaContext.interface.js';
+import postCollectionType from '@/routes/graphql/queryTypes/postQuery/post.collectionType.js';
+import { UserSubscriptionInterface } from '@/routes/graphql/types/prismaSource.interface.js';
 
 const userObjectType = new GraphQLObjectType({
   name: 'User',
@@ -18,12 +23,60 @@ const userObjectType = new GraphQLObjectType({
       type: nonNullableGraphQLFloat,
       description: 'User balance',
     },
-    // TODO: add types for profile, posts, subscriptions, according to type User:
+    profile: {
+      type: profileObjectType,
+      description: 'User Profile',
+      resolve: async (
+        source: User,
+        _: unknown,
+        context: PrismaContextInterface,
+      ): Promise<Profile> => await context.loaders.profileLoader.load(source.id),
+    },
+    posts: {
+      type: postCollectionType,
+      description: 'User Posts',
+      resolve: async (source: User, _: unknown, context: PrismaContextInterface) =>
+        await context.loaders.postLoader.load(source.id),
+    },
+    userSubscribedTo: {
+      type: new GraphQLList(userObjectType),
+      description: 'User Subscription table column',
+      resolve: async (
+        source: UserSubscriptionInterface,
+        _: unknown,
+        context: PrismaContextInterface,
+      ) => {
+        if (source.userSubscribedTo) {
+          const authorIDs: Array<string> = source.userSubscribedTo.map(
+            (user) => user.authorId,
+          );
+          return await context.loaders.userLoader.loadMany(authorIDs);
+        }
+        await context.loaders.userSubscribedToLoader.load(source.id);
+      },
+    },
+    subscribedToUser: {
+      type: new GraphQLList(userObjectType),
+      description: 'User Subscribers table column',
+      resolve: async (
+        source: UserSubscriptionInterface,
+        _: unknown,
+        context: PrismaContextInterface,
+      ) => {
+        if (source.subscribedToUser) {
+          const subscriberIDs: Array<string> = source.subscribedToUser.map(
+            (user) => user.subscriberId,
+          );
+          return await context.loaders.userLoader.loadMany(subscriberIDs);
+        }
+        return await context.loaders.subscribedToUserLoader.load(source.id);
+      },
+    },
   }),
 });
 
 /*
-* type User {
+type User {
   id: UUID!
   name: String!
   balance: Float!
